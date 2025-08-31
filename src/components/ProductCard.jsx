a670bbf6957ca6bfbff343e861da61b7a1fe4334
Zamani079 <222447311@mycput.ac.za>
2025-08-31 18:29:03 +0200

    productcard

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,6 +1,9 @@
-export default function ProductCard({ product }) {
-  return (
-    <div className="border rounded p-4 shadow hover:shadow-lg">
+
+   export default function ProductCard ({ product }) {
+   return(
+   
+   
+   <div className="border rounded p-4 shadow hover:shadow-lg">
       <img src={product.imageURL} alt={product.name} className="w-full h-40 object-cover" />
       <h3 className="mt-2 font-bold">{product.name}</h3>
       <p>{product.brand}</p>
@@ -8,4 +11,4 @@ export default function ProductCard({ product }) {
       <button className="bg-blue-500 text-white px-4 py-2 rounded mt-2">Add to Cart</button>
     </div>
   );
-}
+   }
